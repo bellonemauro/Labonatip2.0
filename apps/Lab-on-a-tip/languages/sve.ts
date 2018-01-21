05295de169cff3cc9bb992961e5aeea6c3e96270
Mauro Bellone <bellonemauro@gmail.com>
2018-01-22 00:18:35 +0100

    more on languages

--- apps/Lab-on-a-tip/languages/sve.ts
+++ apps/Lab-on-a-tip/languages/sve.ts
@@ -59,6 +59,10 @@
         <source>Actions</source>
         <translation>Åtgärder</translation>
 	</message>
+	<message>
+        <source>User :</source>
+        <translation>User :</translation>
+	</message>
     <message>
         <source>ACTIONS</source>
         <translation>ÅTGÄRDER</translation>
@@ -67,10 +71,6 @@
         <source>New tip</source>
         <translation>Ny tipp</translation>
 	</message>	
-    <message>
-        <source>Run protocol</source>
-        <translation>Kör protokoll</translation>
-	</message>		
     <message>
         <source>Operational</source>
         <translation>Operativ</translation>
@@ -136,6 +136,10 @@
         <translation>Info</translation>
 	</message>
 	<message>
+        <source>History</source>
+        <translation>History</translation>
+	</message>
+	<message>
         <source>Waiting ...</source>
         <translation>Vänta ...</translation>
 	</message>
@@ -175,6 +179,314 @@
         <source>Apply</source>
         <translation>Tillämpa</translation>
     </message>		
+	<message>
+        <source>Simulation time</source>
+        <translation>Simulation time</translation>
+	</message>
+	<message>
+        <source>Command history</source>
+        <translation>Command history</translation>
+	</message>
+	<message>
+        <source>Error messages</source>
+        <translation>Error messages</translation>
+	</message>	
+	<message>
+        <source>Enable history</source>
+        <translation>Enable history</translation>
+	</message>
+	<message>
+        <source>Dump to file</source>
+        <translation>Dump to file</translation>
+	</message>
+	<message>
+        <source>Verbose output</source>
+        <translation>Verbose output</translation>
+	</message>
+	<message>
+        <source>Clear history</source>
+        <translation>Clear history</translation>
+	</message>	
+	<message>
+        <source>Out file path</source>
+        <translation>Out file path</translation>
+	</message>	
+	<message>
+        <source>Value</source>
+        <translation>Value</translation>
+	</message>	
+	<message>
+        <source>Outflow ( nL/s )</source>
+        <translation>Outflow ( nL/s )</translation>
+	</message>	
+	<message>
+        <source>Inflow recirc ( nL/s )</source>
+        <translation>Inflow recirc ( nL/s )</translation>
+	</message>	
+	<message>
+        <source>Inflow switch ( nL/s )</source>
+        <translation>Inflow switch ( nL/s )</translation>
+	</message>
+	<message>
+        <source>Ratio</source>
+        <translation>Ratio</translation>
+	</message>	
+	<message>
+        <source>Flow rate 1 ( nL/s )</source>
+        <translation>Flow rate 1 ( nL/s )</translation>
+	</message>	
+	<message>
+        <source>Flow rate 2 ( nL/s )</source>
+        <translation>Flow rate 2 ( nL/s )</translation>
+	</message>	
+	<message>
+        <source>Flow rate 3 ( nL/s )</source>
+        <translation>Flusso 3 ( nL/s )</translation>
+	</message>	
+	<message>
+        <source>Flow rate 4 ( nL/s )</source>
+        <translation>Flow rate 4 ( nL/s )</translation>
+	</message>	
+	<message>
+        <source>Flow rate 5 ( nL/s )</source>
+        <translation>Flow rate 5 ( nL/s )</translation>
+	</message>	
+	<message>
+        <source>Flow rate 6 ( nL/s )</source>
+        <translation>Flow rate 6 ( nL/s )</translation>
+	</message>	
+	<message>
+        <source>Flow rate 7 ( nL/s )</source>
+        <translation>Flow rate 7 ( nL/s )</translation>
+	</message>	
+	<message>
+        <source>Flow rate 8 ( nL/s )</source>
+        <translation>Flow rate 8 ( nL/s )</translation>
+	</message>	
+	<message>
+        <source>Volume in well 1 (uL)</source>
+        <translation>Volume in well 1 (uL)</translation>
+	</message>		
+	<message>
+        <source>Volume in well 2 (uL)</source>
+        <translation>Volume in well 2 (uL)</translation>
+	</message>	
+	<message>
+        <source>Volume in well 3 (uL)</source>
+        <translation>Volume in well 3 (uL)</translation>
+	</message>	
+	<message>
+        <source>Volume in well 4 (uL)</source>
+        <translation>Volume in well 4 (uL)</translation>
+	</message>	
+	<message>
+        <source>Volume in well 5 (uL)</source>
+        <translation>Volume in well 5 (uL)</translation>
+	</message>	
+	<message>
+        <source>Volume in well 6 (uL)</source>
+        <translation>Volume in well 6 (uL)</translation>
+	</message>	
+	<message>
+        <source>Volume in well 7 (uL)</source>
+        <translation>Volume in well 7 (uL)</translation>
+	</message>	
+	<message>
+        <source>Volume in well 8 (uL)</source>
+        <translation>Volume in well 8 (uL)</translation>
+	</message>	
+    <message>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Warning</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <source>PPC1 STATUS: Connected  </source>
+        <translation>PPC1 STATUS: Connected  </translation>
+    </message>
+    <message>
+        <source>PPC1 STATUS: NOT Connected  </source>
+        <translation>PPC1 STATUS: NOT Connected  </translation>
+    </message>
+    <message>
+        <source>No protocol running  </source>
+        <translation>No protocol running  </translation>
+    </message>
+    <message>
+        <source>Disconnect</source>
+        <translation>Disconnect</translation>
+    </message>
+    <message>
+        <source>Cannot save the file</source>
+        <translation>Cannot save the file</translation>
+    </message>
+    <message>
+        <source>Cannot load the file</source>
+        <translation>Cannot load the file</translation>
+    </message>
+    <message>
+        <source>Lab-on-a-tip is in simulation only</source>
+        <translation>Lab-on-a-tip is in simulation only</translation>
+    </message>
+    <message>
+        <source>Lab-on-a-tip could not connect to PPC1, \n please, check cables and settings and press Ok</source>
+        <translation>Lab-on-a-tip could not connect to PPC1, \n please, check cables and settings and press Ok</translation>
+    </message>
+    <message>
+        <source>Lab-on-a-tip could not connect to PPC1 twice, \n please, check cables and settings</source>
+        <translation>Lab-on-a-tip could not connect to PPC1 twice, \n please, check cables and settings</translation>
+    </message>
+    <message>
+        <source>I can try to automatically find the device, \n Should I do it?</source>
+        <translation>I can try to automatically find the device, \n Should I do it?</translation>
+    </message>
+    <message>
+        <source>Lab-on-a-tip connected but PPC1 is not running</source>
+        <translation>Lab-on-a-tip connected but PPC1 is not running</translation>
+    </message>
+    <message>
+        <source>This will stop the PPC1, \n Are you sure?</source>
+        <translation>This will stop the PPC1, \n Are you sure?</translation>
+    </message>
+    <message>
+        <source>Unable to stop and disconnect PPC1</source>
+        <translation>Unable to stop and disconnect PPC1</translation>
+    </message>
+    <message>
+        <source>Shutdown pressed, this will take 30 seconds, press Ok to continue, cancel to abort.</source>
+        <translation>Shutdown pressed, this will take 30 seconds, press Ok to continue, cancel to abort.</translation>
+    </message>
+    <message>
+        <source>The pressure is off, waiting for the vacuum.</source>
+        <translation>The pressure is off, waiting for the vacuum.</translation>
+    </message>    
+	<message>
+        <source>Vacuum off. Stopping the flow in the device.</source>
+        <translation>Vacuum off. Stopping the flow in the device.</translation>
+    </message>    
+	<message>
+        <source>Rebooting ...</source>
+        <translation>Rebooting ...</translation>
+    </message>    
+	<message>
+        <source>Reconnecting ...</source>
+        <translation>Reconnecting ...</translation>
+    </message>
+    <message>
+        <source>Initialization</source>
+        <translation>Initialization</translation>
+    </message>
+    <message>
+        <source>Place the pipette into the holder and tighten. THEN PRESS OK</source>
+        <translation>Place the pipette into the holder and tighten. THEN PRESS OK</translation>
+    </message>
+    <message>
+        <source>Pressurize the system</source>
+        <translation>Pressurize the system</translation>
+    </message>
+    <message>
+        <source>Wait until a droplet appears at the tip of the pipette and THEN PRESS OK</source>
+        <translation>Wait until a droplet appears at the tip of the pipette and THEN PRESS OK</translation>
+    </message>
+    <message>
+        <source>Purging the liquid channels</source>
+        <translation>Purging the liquid channels</translation>
+    </message>    
+	<message>
+        <source>Still purging the liquid channels</source>
+        <translation>Still purging the liquid channels</translation>
+    </message>
+    <message>
+        <source>Remove the droplet using a lens tissue. THEN PRESS OK</source>
+        <translation>Remove the droplet using a lens tissue. THEN PRESS OK</translation>
+    </message>
+    <message>
+        <source>Put the pipette into solution. THEN PRESS OK</source>
+        <translation>Put the pipette into solution. THEN PRESS OK</translation>
+    </message>
+    <message>
+        <source>Purging the vacuum channels</source>
+        <translation>Purging the vacuum channels</translation>
+    </message>    
+	<message>
+        <source>Establishing operational pressures</source>
+        <translation>Establishing operational pressures</translation>
+    </message>
+    <message>
+        <source>Pipette is ready for operation. PRESS OK TO START</source>
+        <translation>Pipette is ready for operation. PRESS OK TO START</translation>
+    </message>    
+	<message>
+        <source>Standby operation progress</source>
+        <translation>Standby operation progress</translation>
+    </message>    
+	<message>
+        <source>Run protocol</source>
+        <translation>Kör protokoll</translation>
+	</message>
+    <message>
+        <source>Stop protocol</source>
+        <translation>Stop protocol</translation>
+    </message>
+    <message>
+        <source>No protocol loaded, load one first</source>
+        <translation>No protocol loaded, load one first</translation>
+    </message>
+    <message>
+        <source>The protocol loaded is : </source>
+        <translation>The protocol loaded is : </translation>
+    </message>
+    <message>
+        <source>\n press ''Ok'' to run the protocol, or press ''Cancel'' to load a new one.</source>
+        <translation>\n press ''Ok'' to run the protocol, or press ''Cancel'' to load a new one.</translation>
+    </message>
+    <message>
+        <source>This operation will take </source>
+        <translation>This operation will take </translation>
+    </message>	
+    <message>
+        <source> seconds.</source>
+        <translation> seconds.</translation>
+    </message>	
+    <message>
+        <source>Ask message command</source>
+        <translation>Ask message command</translation>
+    </message>		
+    <message>
+        <source> No protocol loaded : </source>
+        <translation> No protocol loaded : </translation>
+    </message>	
+    <message>
+        <source> Protocol loaded : </source>
+        <translation> Protocol loaded : </translation>
+    </message>	
+    <message>
+        <source>This will remove all the files in the history folder.\nDo you want to proceed?\n</source>
+        <translation>This will remove all the files in the history folder.\nDo you want to proceed?\n</translation>
+    </message>	
+    <message>
+        <source>History cleaned</source>
+        <translation>History cleaned</translation>
+	</message>
+	<message>
+        <source> PROTOCOL RUNNING : </source>
+        <translation> PROTOCOL RUNNING : </translation>
+	</message>
+	<message>
+        <source> ----- remaining time,  </source>
+        <translation> ----- remaining time,  </translation>
+	</message>
+	<message>
+        <source>Pulse time remaining: </source>
+        <translation>Pulse time remaining: </translation>
+	</message>	
 </context>
 <context>
     <name>Labonatip_tools</name>
@@ -190,9 +502,21 @@
         <source>Solution</source>
         <translation>Lösningar</translation>
     </message>
+     <message>
+        <source>Solutions</source>
+        <translation>Solutions</translation>
+    </message>
     <message>
         <source>Empty wells</source>
         <translation>Töm brunnarna</translation>
+    </message>
+    <message>
+        <source>Empty waste</source>
+        <translation>Empty waste</translation>
+    </message>
+    <message>
+        <source>Refill solutions</source>
+        <translation>Refill solutions</translation>
     </message>
     <message>
         <source>Pressure</source>
@@ -219,6 +543,14 @@
         <translation>Språk</translation>
     </message>
 	<message>
+        <source>Reset to factory defaults</source>
+        <translation>Reset to factory defaults</translation>
+	</message>
+	<message>
+        <source>Enable tool tips</source>
+        <translation>Enable tool tips</translation>
+	</message>
+	<message>
         <source>Volume</source>
         <translation>Volym</translation>
     </message>
@@ -242,6 +574,10 @@
         <source>Vacuum settings</source>
         <translation>Vakuuminställningar</translation>
     </message>	
+	<message>
+        <source>Vacuum settings</source>
+        <translation>Vacuum settings</translation>
+    </message>
     <message>
         <source>Zone size</source>
         <translation>Zonstorlek</translation>
@@ -290,6 +626,90 @@
         <source>Well 8 :</source>
         <translation>Brunn 8 :</translation>
     </message>	
+	<message>
+    <source>Well :</source>
+        <translation>Well :</translation>
+    </message>	
+	<message>
+        <source>Max</source>
+        <translation>Max</translation>
+	</message>
+	<message>
+        <source>Min</source>
+        <translation>Min</translation>
+	</message>
+	<message>
+        <source>Default</source>
+        <translation>Default</translation>
+	</message>
+	<message>
+        <source>COM settings</source>
+        <translation>COM settings</translation>
+	</message>
+	<message>
+        <source>Port name</source>
+        <translation>Port name</translation>
+	</message>
+	<message>
+        <source>Baud rate</source>
+        <translation>Baud rate</translation>
+	</message>
+	<message>
+        <source>Data bit</source>
+        <translation>Data bit</translation>
+	</message>
+	<message>
+        <source>Parity</source>
+        <translation>Parità</translation>
+	</message>
+	<message>
+        <source>Stop bit</source>
+        <translation>Stop bit</translation>
+	</message>
+	<message>
+        <source>Flow control</source>
+        <translation>Flow control</translation>
+	</message>
+	<message>
+        <source>Description:</source>
+        <translation>Description:</translation>
+	</message>
+	<message>
+        <source>Manufacturer:</source>
+        <translation>Manufacturer:</translation>
+	</message>
+	<message>
+        <source>Serial number:</source>
+        <translation>Serial number:</translation>
+	</message>
+	<message>
+        <source>Location:</source>
+        <translation>Location:</translation>
+	</message>
+	<message>
+        <source>Vendor ID:</source>
+        <translation>Vendor ID:</translation>
+	</message>
+	<message>
+        <source>Product ID:</source>
+        <translation>Product ID:</translation>
+	</message>
+	<message>
+        <source>Update available devices</source>
+        <translation>Update available devices</translation>
+	</message>
+	<message>
+        <source>Warning</source>
+        <translation>Warning</translation>
+    </message>
+	<message>
+        <source>This will reset used defined settings and parameters to the factory default value. \nAre you sure ?</source>
+        <translation>This will reset used defined settings and parameters to the factory default value. \nAre you sure ?</translation>
+	</message>
+	<message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+	</message>
 	<message>
         <source>Cancel</source>
         <translation>Avbryt</translation>
